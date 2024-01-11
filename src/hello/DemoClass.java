3720ff62a63ae11ccb9214926e7246bdf6796c6a
SRc <SRc@192.168.1.35>
2024-01-11 23:04:48 +0530

    sum of number

---
+++ src/hello/DemoClass.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5+9;
+		System.out.println("add two number" + a);
+	}
+
+}
